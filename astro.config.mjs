f0a67ed4c8ae5622fedc47cea3d0fb7de31f9fcd
kms0219kms <kms0219kms@naver.com>
2024-11-02 21:45:10 +0700

    :tada: feat: Finish PM maintenance page

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import preact from "@astrojs/preact";
+
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
+export default defineConfig({
+  // Enable Preact to support Preact JSX components.
+  integrations: [preact(), mdx()],
+});
