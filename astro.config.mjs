95994e014d83ed78c66b6ff090fe5068a43c69b0
kms0219kms <kms0219kms@naver.com>
2024-11-02 22:03:11 +0700

    🚀 feat: Deploy to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://pm.lunaiz.com",
   // Enable Preact to support Preact JSX components.
   integrations: [preact(), mdx()],
 });
