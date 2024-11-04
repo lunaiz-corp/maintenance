6865d325b7cb50c0d023f1cf4caf68691c73bc14
kms0219kms <kms0219kms@naver.com>
2024-11-04 12:20:57 +0700

    :recycle: refactor(styles): Refactor the stylesheet logic

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import preact from "@astrojs/preact";
 
+import preact from "@astrojs/preact";
 import mdx from "@astrojs/mdx";
+
+import purgecss from "astro-purgecss";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://pm.lunaiz.com",
   // Enable Preact to support Preact JSX components.
-  integrations: [preact(), mdx()],
+  integrations: [preact(), mdx(), purgecss()],
 });
