c0079292468170fc4dacd0886d5a205b9d3ef78b
kms0219kms <kms0219kms@naver.com>
2024-11-07 20:14:04 +0700

    ⬆ chore(deps): Update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,11 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import preact from "@astrojs/preact";
 import mdx from "@astrojs/mdx";
-
 import purgecss from "astro-purgecss";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://pm.lunaiz.com",
-  // Enable Preact to support Preact JSX components.
-  integrations: [preact(), mdx(), purgecss()],
+  integrations: [mdx(), purgecss()],
 });
